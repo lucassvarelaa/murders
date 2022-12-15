d9f93f14b278206d91e61c9f8e3be97b7aae6fb1
lucasvarelamauro@gmail.com <lucasvarelamauro@gmail.com>
2022-12-15 11:26:21 -0300

    Change on the fontsize in the Barplot

--- analysis.R
+++ analysis.R
@@ -4,6 +4,6 @@ load("rda/murders.rda")
 murders %>% mutate(abb = reorder(abb, rate)) %>%
   ggplot(aes(abb, rate)) +
   geom_bar(width = .5, stat = "identity", color = "black") +
-  coord_flip()
+  coord_flip() + theme(text = element_text(size = 9))
 
 ggsave("figs/barplot.png")
